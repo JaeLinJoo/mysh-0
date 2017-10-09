cb514b937f70e3df329d2087b96dd67fdc928f36
JooJaeLin <c2522507@naver.com>
2017-10-09 15:12:05 +0900

    code commands

--- src/commands.c
+++ src/commands.c
@@ -1,30 +1,47 @@
 #include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
 #include "commands.h"
 
 int do_cd(int argc, char** argv) {
+
   if (!validate_cd_argv(argc, argv))
     return -1;
-
-  // TODO: Fill it!
-  return 0;
+  
+  else 
+      return chdir(argv[1]);
 }
 
 int do_pwd(int argc, char** argv) {
-  if (!validate_pwd_argv(argc, argv))
-    return -1;
+ 
+    if (!validate_pwd_argv(argc, argv))
+      return -1;
 
-  // TODO: Fill it!
-
+char *cmd;
+if((cmd=getcwd(NULL,8096))!=0){
+  printf("%s\n",cmd);
   return 0;
+ }
+ else
+return -1;
 }
 
 int validate_cd_argv(int argc, char** argv) {
-  // TODO: Fill it!
-  return 1;
+
+     if(strcmp(argv[0],"cd")==0&&argc==2)
+	 return 1;
+     else
+        return 0;
+
 }
 
 int validate_pwd_argv(int argc, char** argv) {
-  // TODO: Fill it!
-  return 1;
+ 
+  if(strcmp(argv[0],"pwd")==0&&argc==1)
+     return 1;
+
+  else 
+   return 0;
 }
